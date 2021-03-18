3e499e7a769d85a8a83290e16ab8d498f8b0dfdc
sjoerdvgerwen <63058506+sjoerdvgerwen@users.noreply.github.com>
2021-03-18 10:04:28 +0100

    Initial commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Drip.Application.Entities;
+using Drip.Application.Interfaces;
+using Drip.Webapp.Models;
+
+
+
+
+namespace Drip.Webapp.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IloginRepository _loginRepository;
+
+
+        public LoginController(IloginRepository loginRepository)
+        {
+            _loginRepository = loginRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            List<User> Users = _loginRepository.GetAllUsers();
+
+            var viewModel = new LoginViewModel
+            {
+                users = Users
+            };
+
+            return View(viewModel);
+        }
+
+    }
+}
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Drip.Application.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Drip.Webapp.Models
+{
+    public class LoginViewModel
+    {
+        public List<User> users { get; set; } = new List<User>();
+    }
+}
